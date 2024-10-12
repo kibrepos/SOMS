ec718e86a451077052dabb319f50c39933aaf773
KB <111480118+kibrepos@users.noreply.github.com>
2024-10-12 21:44:15 +0800

    main chat column progress issave lang kz uuwi muna aq

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -2,7 +2,24 @@ import "./chatcss/Chat.css";
 
 const Chat = () => {
     return (
-        <div className='Chat'>Chat</div>
+        <div className='Chat'>
+            <div className="top">
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                <span>Jane Doe</span>
+                <p>Lorem Ipsum dolor, sit amet.</p>
+                </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png" alt="" />
+                    <img src="./video.png" alt="" />
+                    <img src="./info.png" alt="" />
+                </div>
+                </div>
+            <div className="center"></div>
+            <div className="bottom"></div>
+        </div>
     )
 }
 
