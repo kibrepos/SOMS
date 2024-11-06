a1aeec35180b688fe87640092ee13fb9d4259a9a
kibrsifr <kiaobrent@gmail.com>
2024-11-07 01:07:43 +0800

    routep2

--- src/pages/Students/StudentPresidentSidebar.tsx
+++ src/pages/Students/StudentPresidentSidebar.tsx
@@ -87,13 +87,9 @@ const StudentPresidentSidebar: React.FC = () => {
         <NavLink to={`/president/event-management/${organizationName}`} className={({ isActive }) => `student-sidebar-navlink ${isActive ? 'student-active-link' : ''}`}>
           <FontAwesomeIcon icon={faCalendarAlt} /> Event Management
         </NavLink>
-        <NavLink 
-  to={`/Organization/${organizationName}/announcements`} 
-  className={({ isActive }) => `student-sidebar-navlink ${isActive ? 'student-active-link' : ''}`}
->
-  <FontAwesomeIcon icon={faBullhorn} /> Announcements
-</NavLink>
-
+        <NavLink to={`/Organization/${organizationName}/announcements`} className={({ isActive }) => `student-sidebar-navlink ${isActive ? 'student-active-link' : ''}`}>
+        <FontAwesomeIcon icon={faBullhorn} /> Announcements
+        </NavLink>
         <NavLink to={`/president/reporting-analytics/${organizationName}`} className={({ isActive }) => `student-sidebar-navlink ${isActive ? 'student-active-link' : ''}`}>
           <FontAwesomeIcon icon={faChartBar} /> Reporting & Analytics
         </NavLink>
